98c3ad5edfdbeeb87105f482a1824087432e3f5a
Moreshwar Dalvi <moreshwar.dalvi@bicsinfotech.com>
2019-11-11 23:26:28 +0530

    typo changed

--- src/container/membersList/index.jsx
+++ src/container/membersList/index.jsx
@@ -1,61 +1,74 @@
-import React, { Component } from 'react';
-import { Container, Header, Button, Icon, Table } from 'semantic-ui-react';
-import TopHeader from '../../components/TopHeader';
-import './style.css';
-import membersData from './../../components/data/data'
+import React, { Component } from "react";
+import { Container, Header, Button, Icon, Table } from "semantic-ui-react";
+import TopHeader from "../../components/TopHeader";
+import "./style.css";
+import membersData from "./../../components/data/data";
 
 class MemberList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeItem: 'home',
-    }
-  };
+      activeItem: "home"
+    };
+  }
 
   handleItemClick = (e, { name }) => {
     this.setState({ activeItem: name });
-  }
+  };
   render() {
-    console.log(membersData.membersData)
+    console.log(membersData.membersData);
 
     const memberList = membersData.membersData.map((item, index) => {
       return (
         <Table.Row>
           <Table.Cell>{item.name}</Table.Cell>
-          <Table.Cell textAlign='center'>
-            {item.membership_status && <Icon color='green' name='checkmark' size='mediam' />}
+          <Table.Cell textAlign="center">
+            {item.membership_status && (
+              <Icon color="green" name="checkmark" size="mediam" />
+            )}
           </Table.Cell>
-          <Table.Cell textAlign='center'>
+          <Table.Cell textAlign="center">
             {item.meal_allotment.map(a => {
-              return a.morning && <Icon color='green' name='checkmark' size='mediam' />
+              return (
+                a.morning && (
+                  <Icon color="green" name="checkmark" size="mediam" />
+                )
+              );
             })}
           </Table.Cell>
-          <Table.Cell textAlign='center'>
+          <Table.Cell textAlign="center">
             {item.meal_allotment.map(a => {
-              return a.evening && <Icon color='green' name='checkmark' size='mediam' />
+              return (
+                a.evening && (
+                  <Icon color="green" name="checkmark" size="mediam" />
+                )
+              );
             })}
           </Table.Cell>
-          <Table.Cell textAlign='center'>
-            {item.internet_access && <Icon color='green' name='checkmark' size='mediam' />}
+          <Table.Cell textAlign="center">
+            {item.internet_access && (
+              <Icon color="green" name="checkmark" size="mediam" />
+            )}
           </Table.Cell>
         </Table.Row>
-      )
-    })
+      );
+    });
 
     return (
       <div>
         <TopHeader />
         <Container>
-          <Header className="mt-4" as='h3' dividing>
+          <Header className="mt-4" as="h3" dividing>
             Site
             <Button
-              color='black'
-              size='tiny'
-              floated='right'
+              color="black"
+              size="tiny"
+              floated="right"
               animated
-              style={{ width: '100px', marginTop: '-5px' }}>
-              <Button.Content visible >
-                <Icon name='plus squar' />
+              style={{ width: "100px", marginTop: "-5px" }}
+            >
+              <Button.Content visible>
+                <Icon name="plus squar" />
               </Button.Content>
               <Button.Content hidden>Add Member</Button.Content>
             </Button>
@@ -70,10 +83,12 @@ class MemberList extends Component {
           <Table celled structured striped>
             <Table.Header>
               <Table.Row>
-                <Table.HeaderCell rowSpan='2'>Member Name</Table.HeaderCell>
-                <Table.HeaderCell rowSpan='2'>Membership Status</Table.HeaderCell>
-                <Table.HeaderCell colSpan='2'>Languages</Table.HeaderCell>
-                <Table.HeaderCell rowSpan='2'>Intent Access</Table.HeaderCell>
+                <Table.HeaderCell rowSpan="2">Member Name</Table.HeaderCell>
+                <Table.HeaderCell rowSpan="2">
+                  Membership Status
+                </Table.HeaderCell>
+                <Table.HeaderCell colSpan="2">Meal Allotment</Table.HeaderCell>
+                <Table.HeaderCell rowSpan="2">Intent Access</Table.HeaderCell>
               </Table.Row>
               <Table.Row>
                 <Table.HeaderCell>Morning</Table.HeaderCell>
@@ -81,13 +96,11 @@ class MemberList extends Component {
               </Table.Row>
             </Table.Header>
 
-            <Table.Body>
-              {memberList}
-            </Table.Body>
+            <Table.Body>{memberList}</Table.Body>
           </Table>
         </Container>
       </div>
-    )
+    );
   }
 }
 
